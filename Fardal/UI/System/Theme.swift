5714aef055b9a4a6fcd02b432cab1019798f4c26
Tobias Scholze <t.scholze@anfe.ma>
2023-12-24 09:33:36 +0100

    Introduced some theming and commets

--- Fardal/UI/System/Theme.swift
+++ Fardal/UI/System/Theme.swift
@@ -7,16 +7,25 @@
 
 import SwiftUI
 
+/// App-wide theming proeprties
 enum Theme {
+    /// Convinient helper to access colors
+    /// Use default `Image(:resources)` if a simple color access is required
     enum Colors {
         static let pastelColors: [Color] = [.pastelRed, .pastelBlue, .pastelPink, .pastelBrown, .pastelGreen, .pastelOrange, .pastelPurple, .pastelYellow]
     }
 
+    /// Unified shapes
     enum Shape {
+        /// Level 1 rounded rectangle
         static let roundedRectangle1 = RoundedRectangle(cornerRadius: Theme.cornerRadius1)
+        
+        /// Level 2 rounded rectangle. Use it if parent has `roundedRectangle1`
         static let roundedRectangle2 = RoundedRectangle(cornerRadius: Theme.cornerRadius2)
     }
 
+    // MARK: - Private properties -
+    
     static let cornerRadius1: CGFloat = 8
     static let cornerRadius2: CGFloat = 4
 }
